5affc31db0a572fc47fbeae4bf1d7732639bcf2c
billydolan <83192688+billydolan@users.noreply.github.com>
2024-02-08 11:31:19 -0500

    Uploaded Current Files
    Uploaded Current Hws: 1-3,
    
    Project environment for the T-37A,

---
+++ TA37 Simulink Project/Input.m
@@ -0,0 +1,38 @@
+m = 0;
+g = -9.81;
+Ftx = 0;
+Fax = 0;
+Fty = 0;
+Fay = 0;
+Ftz = 0;
+Faz = 0;
+Ft = [Ftx; Fty; Ftz];
+Fa = [Fax; Fay; Faz]; 
+Forces = Ft+Fa; 
+
+theta = 0;
+psi = 0;
+phi = 0;
+P=1;
+Q=1;
+W=1;
+R=1;
+V=1;
+U=1;
+
+u_dot=0;
+v_dot=0;
+w_dot=0;
+
+velocity = 100;
+t = 10;
+
+%modelname = 'T37_SimulinkModel';
+%sim(modelname)
+
+%moments
+Mx = 0;
+My = 0;
+Mz = 0;
+
+M = [Mx; My; Mz];

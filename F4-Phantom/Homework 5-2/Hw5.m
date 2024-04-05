6502d05a9e985ff47c02a46822377729a97c4ea3
Billy <83192688+billydolan@users.noreply.github.com>
2024-04-05 17:41:16 -0400

    Update Hw5.m

--- F4-Phantom/Homework 5-2/Hw5.m
+++ F4-Phantom/Homework 5-2/Hw5.m
@@ -1,17 +1,18 @@
 % Call the Project 1 Matlab File, but make it so it's only the coefficients
 % in a seperate file 
-
+F4ModelCode
 
 %% 
 
 % Small perturbation Assumptions
 
 %Z's 
-
+Z_alpha = - q1*S*(CL_alpha + CDi_1) / m;
+Z_u = -(qbar1*S*(c_L_u+(2*c_L_1)))/(m*U1)
+%M's 
 
 M_alphadot = (c_m_alphadot*qbar1*S*(cbar^2)) / (2*IYY*V_p_1);
-Mq = (c_m_q*qbar1*S*(cbar^2)) / (2*IYY*V_p_1);
-Ma_alpha = cm_alpha*q1*S*c / Iyy;
-Za = - q1*S*(CL_alpha + CDi_1) / m;
+M_q = (c_m_q*qbar1*S*(cbar^2)) / (2*IYY*V_p_1);
+M_alpha = (c_m_alpha*qbar1*S*cbar) / IYY;
 
 %Natural Frequencies
